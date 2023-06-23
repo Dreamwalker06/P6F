2f44037f4025e17c3a05a0bf06fea48176b49744
DreamWalker06 <kiru59@hotmail.fr>
2023-06-23 14:05:57 +0200

    Création du composant Collapse, Page à propos terminé

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,6 +3,7 @@ import Navigation from "../components/Navigation";
 import Footer from "../components/Footer";
 import Banner from "../components/Banner";
 import backgroundImage from "../assets/images/kalen-emsley-Bkci_8qcdvQ-unsplash2.webp";
+import Collapse from "../components/Collapse";
 
 const About = () => {
   return (
@@ -14,6 +15,35 @@ const About = () => {
         className="banner"
         filter="brightness(70%)"
       />
+      <div className="collapse">
+        <Collapse
+          title="Fiabilité"
+          text="Les annonces postées sur Kasa garantissent une fiabilité totale. Les
+            photos sont conformes aux logements, et toutes les informations sont
+            régulièrement vérifiées par nos équipes."
+        />
+        <Collapse
+          title="Respect"
+          text="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
+            comportement discrminatoire ou de perturbation du voisinage
+            entraînera une exclusion de notre plateforme."
+        />
+        <Collapse
+          title="Service"
+          text="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
+            comportement discrminatoire ou de perturbation du voisinage
+            entraînera une exclusion de notre plateforme."
+        />
+        <Collapse
+          title="Sécurité"
+          text="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que
+            pour les voyageurs, chaque logement correspond aux critères de
+            sécurité établis par nos services. En laissant une note aussi bien à
+            l'hôte qu'au locataire, cela permet à nos équipes de vérifier que
+            les standards sont bien respectés. Nous organisons également des
+            ateliers sur la sécurité domestique pour nos hôtes"
+        />
+      </div>
       <Footer />
     </div>
   );
