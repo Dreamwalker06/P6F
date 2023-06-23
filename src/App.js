8ca63ea8a7009461e3d47afa81fe1744c4c4970e
DreamWalker06 <kiru59@hotmail.fr>
2023-06-23 11:47:02 +0200

    Ajout du composant Header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import "./App.scss";
+import "./assets/styles/App.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Error404 from "./pages/Error404";
+import About from "./pages/About";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<Error404 />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
